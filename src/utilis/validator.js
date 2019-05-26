80028cdae1a4e78c1916f2ff36cb25f1172c0448
hariclerry <clerryha@gmail.com>
2019-05-26 14:19:46 +0300

    add integration test

--- src/utilis/validator.js
+++ src/utilis/validator.js
@@ -2,9 +2,16 @@ const Joi = require('joi');
 
 function validateRecord(record) {
   const schema = {
-    locationName: Joi.string().min(3).max(50).required(),
-    numberOfFemale: Joi.number().integer().required(),
-    numberOfMale: Joi.number().integer().required(),
+    locationName: Joi.string()
+      .min(3)
+      .max(50)
+      .required(),
+    numberOfFemale: Joi.number()
+      .integer()
+      .required(),
+    numberOfMale: Joi.number()
+      .integer()
+      .required()
   };
 
   return Joi.validate(record, schema);
